593e34977fa9e7595dbf2ff63b4bb1053c65586c
DenisRomanovsky <denys.r@stadiumgoods.com>
2020-01-24 13:46:38 +0300

    Row.

--- stardb/src/components/app/app.js
+++ stardb/src/components/app/app.js
@@ -61,8 +61,8 @@ export default class App extends Component {
         <div className="row mb2">
           <div className="col-md-6">
             <ItemList
-              onItemSelected={this.onPersonSelected}
-              getData={this.swapiService.getAllPlanets} />
+              getData={this.swapiService.getAllPlanets}
+              renderItem={(item) => item.name }/>
           </div>
           <div className="col-md-6">
             <PersonDetails personId={this.state.selectedPerson} />
@@ -72,8 +72,8 @@ export default class App extends Component {
         <div className="row mb2">
           <div className="col-md-6">
             <ItemList
-              onItemSelected={this.onPersonSelected}
-              getData={this.swapiService.getAllStarships} />
+              getData={this.swapiService.getAllStarships}
+              renderItem={(item) => item.name}/>
           </div>
           <div className="col-md-6">
             <PersonDetails personId={this.state.selectedPerson} />
--- stardb/src/components/item-list/item-list.js
+++ stardb/src/components/item-list/item-list.js
@@ -23,12 +23,14 @@ export default class ItemList extends Component {
   }
 
   renderItems(arr) {
-    return arr.map(({id, name}) => {
+    return arr.map((item) => {
+      const label = this.props.renderItem(item);
+      const {id} = item;
       return (
         <li className="list-group-item"
             key={id}
             onClick={() => this.props.onItemSelected(id)}>
-          {name}
+          {label}
         </li>
       );
     });
